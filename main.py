6040a088e58ef89d5cbfc28953d97ddda47a74e4
D0kRay <pasi.k11@web.de>
2022-12-12 10:36:46 +0100

    GUI Tabs erweitert

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtGui import QIcon
 from qt_material import apply_stylesheet
-from MainWindow import Ui_MainWindow
+from ui_MainWindow import Ui_MainWindow
 import sys
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-
-
+    
+    
 app = QtWidgets.QApplication(sys.argv)
 
-apply_stylesheet(app, theme='dark_teal.xml')
+apply_stylesheet(app, theme='dark_cyan.xml')
 
 window = MainWindow()
 window.setWindowIcon(QIcon("UEB_icon.png"))
-window.setWindowTitle("UEB Tester")
+window.setWindowTitle("UEB")
 window.show()
 app.exec()
