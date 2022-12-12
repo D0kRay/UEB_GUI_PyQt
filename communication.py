aee6ce203039c753d3655673a4114adc07c689b5
D0kRay <pasi.k11@web.de>
2022-12-12 13:30:53 +0100

    pyserial eingebunden. ComboBox wird nun mit verfügbaren Com Ports befüllt.

---
+++ communication.py
@@ -0,0 +1,30 @@
+import sys
+import serial
+import serial.tools.list_ports
+
+
+class Communication:
+
+    serial_handler = serial 
+    ports = list
+
+    def __init__(self) -> None:
+        pass
+
+        
+    def getComPorts(self):
+        self.ports = serial.tools.list_ports.comports()
+
+        for port, desc, hwid in sorted(self.ports):
+            print("{}: {} [{}]".format(port, desc, hwid))
+
+        portlist = []
+
+        for port in sorted(self.ports):
+            portlist.append(port.name)
+
+        return portlist
+
+
+
+
